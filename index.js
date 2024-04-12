71f3c61148732694f7bab96a4c60070da31a29f5
Cristian <cri.jimenez24@gmail.com>
2024-04-12 15:28:58 -0400

    Consumo de array de objetos externo y se agregaron rutas absolutas en archivos estaticos

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 
+import { services } from './data/services.data.js';
+import path from 'path';
+
 const app = express();
 const PORT = 5000;
 
+const __dirname = import.meta.dirname;
+
 // public directory 
-app.use(express.static('public'));
-app.use('/css', express.static('node_modules/bootstrap/dist/css'));
-app.use('/js', express.static('node_modules/bootstrap/dist/js'));
-app.use('/js', express.static('node_modules/jquery/dist'));
+app.use(express.static( path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname,'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname,'node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname,'node_modules/jquery/dist')));
 
 
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
-app.set('views', './views');
+app.set('views', path.join(__dirname,'/views'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home' , {title: "Home Page 2.0"});
 });
 
 app.get('/services', (req, res) => {
-    res.render('services');
+    res.render('services', {services});
 });
+
+app.get('/services/:name' , (req, res) => {
+
+    const nameURL = req.params.name;
+    const service = services.find(item => item.url === `/services/${nameURL}`)
+    res.render('service', {service});
+})
 
 app.listen(PORT, () => {
     console.log('Estoy levantando el servidor en http://localhost:',PORT);
