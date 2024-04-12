b7ad24c888e764053f4b4adad13a0a6db0462c73
Cristian <cri.jimenez24@gmail.com>
2024-04-12 13:04:54 -0400

    configuracion de archivos hbs y estaticos, instalacion de jquery y boostrap

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import express from 'express';
+import { engine } from 'express-handlebars';
 
 const app = express();
 const PORT = 5000;
 
-app.get('/', function (req ,res) {
-    res.send('Hola');
-})
+// public directory 
+app.use(express.static('public'));
+app.use('/css', express.static('node_modules/bootstrap/dist/css'));
+app.use('/js', express.static('node_modules/bootstrap/dist/js'));
+app.use('/js', express.static('node_modules/jquery/dist'));
+
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/services', (req, res) => {
+    res.render('services');
+});
 
 app.listen(PORT, () => {
-    console.log('Estoy levantando el servidor en localhost:',PORT);
+    console.log('Estoy levantando el servidor en http://localhost:',PORT);
 })
